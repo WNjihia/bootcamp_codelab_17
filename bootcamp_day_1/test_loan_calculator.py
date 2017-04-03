587ac8ed42764202a6e63cd7857454faf8e3a4cd
WNjihia <dorisn172@gmail.com>
2017-04-03 22:54:51 +0300

    add test to check type for one argument

--- bootcamp_day_1/test_loan_calculator.py
+++ bootcamp_day_1/test_loan_calculator.py
@@ -12,8 +12,17 @@ class Loan_calculator(unittest.TestCase):
     def test_returns_error_msg__if_negative_amount(self):
         self.assertEquals(loancalculator(-100000, 12, 10), 'Invalid amount')
 
-    def test_returns_error_message_if_args_not_numbers(self):
+    def test_returns_error_message_if_args_not_variable_type(self):
         self.assertRaises(ValueError, loancalculator, 'twenty', 'three', 'ten')
+
+    def test_returns_error_message_if_amount_arg_not_variable_type(self):
+        self.assertRaises(ValueError, loancalculator, 'twenty', 12, 10)
+
+    def test_returns_error_message_if_time_arg_not_variable_type(self):
+        self.assertRaises(ValueError, loancalculator, 200000, 'three', 10)
+
+    def test_returns_error_message_if_rate_arg_not_variable_type(self):
+        self.assertRaises(ValueError, loancalculator, 50000, 6, 'ten')
 
 if __name__ == '__main__':
     unittest.main()
