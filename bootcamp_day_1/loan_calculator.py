9f986080e5a52741cccd3e492fb1e547ff0812c0
WNjihia <dorisn172@gmail.com>
2017-04-03 22:12:19 +0300

    refactor loan_calculator

--- bootcamp_day_1/loan_calculator.py
+++ bootcamp_day_1/loan_calculator.py
@@ -1,9 +1,12 @@
 def loancalculator(amount, time, rate):
-    if time < 1:
-        return 'Invalid month'
+    if amount > 0:
+        if time < 1:
+            return 'Invalid month'
+        else:
+            repayable_amount = amount + (amount*(rate/100)*(time/12))
+            return repayable_amount
     else:
-        repayable_amount = amount + (amount*(rate/100)*(time/12))
-        return repayable_amount
+        return 'Invalid amount'
 
 
 print(loancalculator(100000, 12, 12))
