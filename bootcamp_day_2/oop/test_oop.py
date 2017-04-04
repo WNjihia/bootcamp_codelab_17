2e282c922de9c1b8dd1a1d6ad5305cfbfbca9d9e
WNjihia <dorisn172@gmail.com>
2017-04-04 16:03:22 +0300

    add test to check object type

--- bootcamp_day_2/oop/test_oop.py
+++ bootcamp_day_2/oop/test_oop.py
@@ -9,5 +9,9 @@ class oopTest(unittest.TestCase):
         owner1 = Property_owner('Ian', 'Smith', [tenant1])
         self.assertIsInstance(owner1, Person, msg='The object should be an instance of the `Person` class')
 
+    def test_object_type(self):
+        tenant1 = Tenant('Sue', 'Jackson', 10000)
+        self.assertTrue((type(tenant1) is Tenant), msg='The object should be a type of `Tenant`')
+
 if __name__ == '__main__':
     unittest.main()
