f6e44209b0cf72ad15f3225629fafa23f5bcb4ba
WNjihia <dorisn172@gmail.com>
2017-04-04 07:08:29 +0300

    add condition for value is greater than one

--- bootcamp_day_1/prime_numbers/prime_number.py
+++ bootcamp_day_1/prime_numbers/prime_number.py
@@ -1,13 +1,14 @@
 def gen_prime_number(value):
     prime_numbers_list = []
-    for element in range(2, value+1):
-        isPrime = True
-        for num in range(2, element):
-            if element % num == 0:
-                isPrime = False
-        if isPrime:
-            prime_numbers_list.append(element)
+    if value > 1:
+        for element in range(2, value+1):
+            isPrime = True
+            for num in range(2, element):
+                if element % num == 0:
+                    isPrime = False
+            if isPrime:
+                prime_numbers_list.append(element)
     return prime_numbers_list
 
 
-print(gen_prime_number(10))
+print(gen_prime_number(2))
