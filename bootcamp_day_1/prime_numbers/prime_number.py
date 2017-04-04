e93ecd35cac64055f020ccf542cc33eade50c4e2
WNjihia <dorisn172@gmail.com>
2017-04-04 07:31:21 +0300

    add condition to check for variable type

--- bootcamp_day_1/prime_numbers/prime_number.py
+++ bootcamp_day_1/prime_numbers/prime_number.py
@@ -1,15 +1,18 @@
 def gen_prime_number(value):
-    prime_numbers_list = []
-    if value > 1:
-        for element in range(2, value+1):
-            isPrime = True
-            for num in range(2, element):
-                if element % num == 0:
-                    isPrime = False
-            if isPrime:
-                prime_numbers_list.append(element)
+    if isinstance(value, int):
+        prime_numbers_list = []
+        if value > 1:
+            for element in range(2, value+1):
+                isPrime = True
+                for num in range(2, element):
+                    if element % num == 0:
+                        isPrime = False
+                if isPrime:
+                    prime_numbers_list.append(element)
+        else:
+            return 'Invalid input'
     else:
-        return 'Invalid input'
+        raise ValueError
     return prime_numbers_list
 
 
