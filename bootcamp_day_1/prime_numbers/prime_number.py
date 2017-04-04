911dcc8b8e47067444934ac81d048dbbf6a64731
WNjihia <dorisn172@gmail.com>
2017-04-04 07:50:09 +0300

    add comments

--- bootcamp_day_1/prime_numbers/prime_number.py
+++ bootcamp_day_1/prime_numbers/prime_number.py
@@ -1,7 +1,14 @@
 def gen_prime_number(value):
+    """
+    function to generate a list of prime numbers within a certain range
+
+    args: value which is the upper limit for the range
+
+    returns a list of prime numbers
+    """
     if isinstance(value, int):
-        prime_numbers_list = []
         if value > 1:
+            prime_numbers_list = []
             for element in range(2, value+1):
                 isPrime = True
                 for num in range(2, element):
