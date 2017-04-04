1f7b755ebdc4157b7c55c370547e3af0558159b3
WNjihia <dorisn172@gmail.com>
2017-04-04 07:09:50 +0300

    add tests for values less than one

--- bootcamp_day_1/prime_numbers/test_prime_number.py
+++ bootcamp_day_1/prime_numbers/test_prime_number.py
@@ -6,5 +6,12 @@ class Generate_prime_numbers(unittest.TestCase):
     def test_it_works(self):
         self.assertEquals(gen_prime_number(10), [2, 3, 5, 7])
 
+    def test_returns_error_msg_if_value_is_negative(self):
+        self.assertEquals(gen_prime_number(-10), 'Invalid input')
+
+    def test_returns_error_msg_if_value_is_zero(self):
+        self.assertEquals(gen_prime_number(0), 'Invalid input')
+
+
 if __name__ == '__main__':
     unittest.main()
