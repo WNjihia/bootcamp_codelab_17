edc3fd627e4be5dd10fbd989059b1cfb72623401
WNjihia <dorisn172@gmail.com>
2017-04-04 12:23:12 +0300

    add docstring

--- bootcamp_day_2/Andelabs/fizzbuzz/fizzbuzz.py
+++ bootcamp_day_2/Andelabs/fizzbuzz/fizzbuzz.py
@@ -1,4 +1,10 @@
 def fizz_buzz(num):
+    """
+    function to return 'Fizz', 'Buzz', 'FizzBuzz' or the argument
+    depending on the argument of the function being divisible by
+    3, 5 or both 3 and 5 repectively
+
+    """
     if (num % 3 == 0) and (num % 5 != 0):
         return 'Fizz'
     elif (num % 3 != 0) and (num % 5 == 0):
