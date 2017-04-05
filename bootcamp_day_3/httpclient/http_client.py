4a759826a946ec03a7ff9e69af4e27916e943e51
WNjihia <dorisn172@gmail.com>
2017-04-05 16:43:34 +0300

    remove tanzania from travel guide

--- bootcamp_day_3/httpclient/http_client.py
+++ bootcamp_day_3/httpclient/http_client.py
@@ -33,18 +33,6 @@ def weather(country, city):
         humidity = weather_data.json()['current_observation']['relative_humidity']
         local_time = weather_data.json()['current_observation']['local_time_rfc822']
         print("Temperature:" + str(temp), "Humidity:" + str(humidity), "Local time:" + str(local_time))
-    elif (country == '3') and (city == '1'):
-        weather_data = requests.get("http://api.wunderground.com/api/b9f730afe2b552a5/conditions/q/Tanzania/Dodoma.json")
-        temp = weather_data.json()['current_observation']['temp_c']
-        humidity = weather_data.json()['current_observation']['relative_humidity']
-        local_time = weather_data.json()['current_observation']['local_time_rfc822']
-        print("Temperature:" + str(temp), "Humidity:" + str(humidity), "Local time:" + str(local_time))
-    elif (country == '3') and (city == '2'):
-        weather_data = requests.get("http://api.wunderground.com/api/b9f730afe2b552a5/conditions/q/Tanzania/Arusha.json")
-        temp = weather_data.json()['current_observation']['temp_c']
-        humidity = weather_data.json()['current_observation']['relative_humidity']
-        local_time = weather_data.json()['current_observation']['local_time_rfc822']
-        print("Temperature:" + str(temp), "Humidity:" + str(humidity), "Local time:" + str(local_time))
     else:
         print('Invalid input')
 
@@ -60,15 +48,12 @@ def currency(input_value):
     if input_value == '1':
         converted_currency = curr_data.json()['rates']['KES']
         print("1 USD = " + str(converted_currency))
-    elif input_value == '2':
-        converted_currency = curr_data.json()['rates']['UGX']
-        print("1 USD = " + str(converted_currency))
     else:
-        converted_currency = curr_data.json()['rates']['TZS']
+        converted_currency = curr_data.json()['rates']['UGX']
         print("1 USD = " + str(converted_currency))
 
 def travel_guide():
-    country_input = input('\n\t\tTRAVEL EAST AFRICA\n\t\t\tWELCOME\n\tPlease select a country:\n\t1. Kenya\n\t2. Uganda\n\t3. Tanzania\n\t')
+    country_input = input('\n\t\tTRAVEL EAST AFRICA\n\t\t\tWELCOME\n\tPlease select a country:\n\t1. Kenya\n\t2. Uganda\n')
     if country_input == '1':
         city_input = input('\tPlease select city\n\t1. Nairobi\n\t2. Kisumu\n')
         if city_input == '1':
@@ -91,20 +76,6 @@ def travel_guide():
             currency(country_input)
         elif city_input == '2':
             city = 'entebbe'
-            geometry(city)
-            currency(country_input)
-            weather(country_input, city_input)
-        else:
-            print('Invalid input')
-    elif country_input == '3':
-        city_input = input('\tPlease select city\n\t1. Dodoma\n\t2. Arusha\n')
-        if city_input == '1':
-            city = 'dodoma'
-            geometry(city)
-            currency(country_input)
-            weather(country_input, city_input)
-        elif city_input == '2':
-            city = 'arusha'
             geometry(city)
             currency(country_input)
             weather(country_input, city_input)
