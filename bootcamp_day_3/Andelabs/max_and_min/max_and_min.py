641cf3e4815a3a81d76add03e2c9a64b5acca3a2
WNjihia <dorisn172@gmail.com>
2017-04-05 09:36:56 +0300

    add function for max and min list values

---
+++ bootcamp_day_3/Andelabs/max_and_min/max_and_min.py
@@ -0,0 +1,12 @@
+def find_max_min(list_value):
+    if len(set(list_value)) > 1:
+        return [min(list_value), max(list_value)]
+    else:
+        return [list_value[0]]
+
+
+print(find_max_min([1, 2, 3, 4]))
+print(find_max_min([6, 4]))
+print(find_max_min([4, 66, 6, 44, 7, 78, 8, 68, 2]))
+print(find_max_min([1, 2, 3, 4]))
+print(find_max_min([4, 4, 4, 4]))
