314e1d85cd3e5f66e3f80f693a7d16eea956333c
WNjihia <dorisn172@gmail.com>
2017-04-05 17:05:49 +0300

    remove sensitive information

--- bootcamp_day_3/httpclient/http_client.py
+++ bootcamp_day_3/httpclient/http_client.py
@@ -1,5 +1,6 @@
 import requests
 import googlemaps
+from api_keys import *
 
 
 def run():
@@ -9,26 +10,27 @@ def run():
        travel_guide()
 
 def weather(country, city):
+    weather_api_key = weather_API_KEY
     if (country == '1') and (city == '1'):
-        weather_data = requests.get("http://api.wunderground.com/api/b9f730afe2b552a5/conditions/q/Kenya/Nairobi.json")
+        weather_data = requests.get("http://api.wunderground.com/api/weather_api_key/conditions/q/Kenya/Nairobi.json")
         temp = weather_data.json()['current_observation']['temp_c']
         humidity = weather_data.json()['current_observation']['relative_humidity']
         local_time = weather_data.json()['current_observation']['local_time_rfc822']
         print("Temperature:" + str(temp), "Humidity:" + str(humidity), "Local time:" + str(local_time))
     elif (country == '1') and (city == '2'):
-        weather_data = requests.get("http://api.wunderground.com/api/b9f730afe2b552a5/conditions/q/Kenya/Kisumu.json")
+        weather_data = requests.get("http://api.wunderground.com/api/weather_api_key/conditions/q/Kenya/Kisumu.json")
         temp = weather_data.json()['current_observation']['temp_c']
         humidity = weather_data.json()['current_observation']['relative_humidity']
         local_time = weather_data.json()['current_observation']['local_time_rfc822']
         print("Temperature:" + str(temp), "Humidity:" + str(humidity), "Local time:" + str(local_time))
     elif (country == '2') and (city == '1'):
-        weather_data = requests.get("http://api.wunderground.com/api/b9f730afe2b552a5/conditions/q/Uganda/Kampala.json")
+        weather_data = requests.get("http://api.wunderground.com/api/weather_api_key/conditions/q/Uganda/Kampala.json")
         temp = weather_data.json()['current_observation']['temp_c']
         humidity = weather_data.json()['current_observation']['relative_humidity']
         local_time = weather_data.json()['current_observation']['local_time_rfc822']
         print("Temperature:" + str(temp), "Humidity:" + str(humidity), "Local time:" + str(local_time))
     elif (country == '2') and (city == '2'):
-        weather_data = requests.get("http://api.wunderground.com/api/b9f730afe2b552a5/conditions/q/Uganda/Entebbe.json")
+        weather_data = requests.get("http://api.wunderground.com/api/weather_api_key/conditions/q/Uganda/Entebbe.json")
         temp = weather_data.json()['current_observation']['temp_c']
         humidity = weather_data.json()['current_observation']['relative_humidity']
         local_time = weather_data.json()['current_observation']['local_time_rfc822']
@@ -37,14 +39,16 @@ def weather(country, city):
         print('Invalid input')
 
 def geometry(input_value):
-    gm = googlemaps.Client(key='AIzaSyDMngKtxnUgPQAwD80_e0IsvHAnx-6Xuw8')
+    google_api_key = google_API_KEY
+    gm = googlemaps.Client(key=api_key)
     geocode_result = gm.geocode(input_value)[0]
     latitude = geocode_result['geometry']['location']['lat']
     longitude = geocode_result['geometry']['location']['lng']
     print("Latitude:" + str(latitude), "Longitude" + str(longitude))
 
 def currency(input_value):
-    curr_data = requests.get('https://openexchangerates.org/api/latest.json?app_id=a3db7dbfca7744ca93488b91fdd84eba')
+    currency_api_key = currency_API_KEY
+    curr_data = requests.get('https://openexchangerates.org/api/latest.json?app_id=currency_api_key')
     if input_value == '1':
         converted_currency = curr_data.json()['rates']['KES']
         print("1 USD = " + str(converted_currency))
